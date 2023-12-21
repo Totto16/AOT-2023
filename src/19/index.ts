7d85b887981935face7837ccd2496c7a29c7001f
Totto16 <tobiausgais@gmail.com>
2023-12-21 15:59:48 +0100

    add day 19

---
+++ src/19/index.ts
@@ -0,0 +1,27 @@
+type Length<T extends unknown[]> = T["length"]
+type Succ<T extends unknown[]> = [...T, ""]
+
+type GenerateSingle<
+	T extends number,
+	S,
+	Count extends any[] = []
+> = Length<Count> extends T ? [] : [S, ...GenerateSingle<T, S, Succ<Count>>]
+
+type Generate<
+	T extends number[],
+	S extends any[],
+	P extends any[] = []
+> = T extends [infer A1, ...infer A2]
+	? A1 extends number
+		? A2 extends number[]
+			? S[Length<P>] extends undefined
+				? [...GenerateSingle<A1, S[0]>, ...Generate<A2, S, Succ<[]>>]
+				: [
+						...GenerateSingle<A1, S[Length<P>]>,
+						...Generate<A2, S, Succ<P>>
+				  ]
+			: never
+		: never
+	: []
+
+type Rebuild<T extends number[]> = Generate<T, ["🛹", "🚲", "🛴", "🏄"]>
